01c39e55ee523aaf1316f61a09c692a6ccb939ab
Topan Sidiq <topansidiq28@gmail.com>
2024-07-23 12:49:59 +0700

    Create ItterableApp.java

---
+++ belajar_java_collection/topan/collection/ItterableApp.java
@@ -0,0 +1,16 @@
+package topan.collection;
+
+public class ItterableApp {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static class Itterables {
+    private Itterables[] itterables;
+
+    public Itterables(Itterables[] itterables) {
+      this.itterables = itterables;
+    }
+  }
+}
