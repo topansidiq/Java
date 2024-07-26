b4e8e9a3e6999f0305d79ffc51e2be20611f660e
Topan Sidiq <topansidiq28@gmail.com>
2024-07-26 14:21:42 +0700

    Istirahat sholat dan makan siang

--- belajar_java_unit_test/src/test/java/topan/CalculatorTest.java
+++ belajar_java_unit_test/src/test/java/topan/CalculatorTest.java
@@ -1,11 +1,16 @@
 package topan;
 
 import org.junit.jupiter.api.Test;
+import org.opentest4j.TestAbortedException;
 
 import topan.generator.SimpleDisplayNameGenerator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayNameGeneration;
 
@@ -13,6 +18,26 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 @DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
   private Calculator calculator = new Calculator();
+
+  @BeforeAll
+  public static void beforeAll() {
+    System.out.println("Before all");
+  }
+
+  @AfterAll
+  public static void afterAll() {
+    System.out.println("After all");
+  }
+
+  @BeforeEach
+  public void setUp() {
+    System.out.println("Before");
+  }
+
+  @AfterEach
+  public void tearDown() {
+    System.out.println("After");
+  }
 
   // @DisplayName("Test Function Add Calculator Success")
   @Test
@@ -39,5 +64,13 @@ public class CalculatorTest {
   @Disabled
   public void testComingSoon() {
 
+  }
+
+  @Test
+  public void testAborted() {
+    var profile = System.getenv("PROFILE");
+    if (!"DEV".equals(profile)) {
+      throw new TestAbortedException("Test di batalkan karena bukan dev!");
+    }
   }
 }
