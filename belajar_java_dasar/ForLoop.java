f5d9f6f2b9a7aa29dd1e7af2b30d2905a164bc49
Topan Sidiq <topansidiq28@gmail.com>
2024-08-22 00:23:50 +0700

    Mulai belajar lagi

--- belajar_java_dasar/ForLoop.java
+++ belajar_java_dasar/ForLoop.java
@@ -1,9 +1,58 @@
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.Set;
+
 public class ForLoop {
     public static void main(String[] args) {
 
-        /// For Loop
-        for (var i = 0; i < 10; i++) {
-            System.out.println(i + 1 + ". Halo!");
+        // /// For Loop
+        // for (var i = 0; i < 10; i++) {
+        // System.out.println(i + 1 + ". Halo!");
+        // }
+
+        // System.out.println(1);
+        // System.out.println(2);
+        // System.out.println(3);
+        // System.out.println(4);
+        // System.out.println(5);
+        // System.out.println(6);
+        // System.out.println(7);
+        // System.out.println(8);
+
+        // System.out.println(1 + ". Angka ke-" + 1);
+        // System.out.println(2 + ". Angka ke-" + 2);
+
+        Set<Currency> currencies = Currency.getAvailableCurrencies();
+        currencies.forEach(currency -> {
+            System.out.println(currency);
+            if (currency.toString().equals("IDR")) {
+                System.out.println(currency.getDisplayName());
+            }
+        });
+
+        for (int i = 1; i <= 100; i++) {
+
+            LocalDate localDate = LocalDate.now();
+            if (i == localDate.getMonthValue()) {
+                System.out.println("Sekarang bulan ke-" + i + ", yaitu bulan " + localDate.getMonth());
+            }
+            if (i == localDate.getDayOfMonth()) {
+                System.out.println("Sekarang tanggal " + i);
+                continue;
+            }
+
+            if (i % 10 == 0) {
+                continue;
+            } else if (i % 5 == 0) {
+                int nilaiBaru = i * 5;
+                System.out.println(nilaiBaru + " dikali 5");
+                continue;
+            } else if (i % 2 == 0) {
+                System.out.println(i + " Genap");
+            } else {
+                System.out.println(i + " Ganjil");
+            }
+
         }
     }
 }
--- belajar_java_dasar/SwitchStatement.java
+++ belajar_java_dasar/SwitchStatement.java
@@ -1,35 +1,52 @@
 public class SwitchStatement {
     public static void main(String[] args) {
-        char nilai = 'A';
+        // char nilai = 'A';
 
-        switch (nilai) {
+        // switch (nilai) {
+        // case 'A':
+        // System.out.println("Anda lulus sempurna");
+        // break;
+        // case 'B':
+        // System.out.println("Anda Lulus!");
+        // break;
+        // default:
+        // System.out.println("Anda salah masuk!");
+        // break;
+        // }
+
+        // /// Switch Lambda
+        // switch (nilai) {
+        // case 'A' -> System.out.println("Anda lulus!");
+        // default -> {
+        // System.out.println("Anda tidak lulus!");
+        // }
+        // }
+
+        // /// Keyword yield
+        // String ucapan = switch (nilai) {
+        // case 'A':
+        // yield "Wah, anda lulus sempurna hebat sekali!";
+        // default:
+        // yield "Okelah!";
+        // };
+
+        // System.out.println(ucapan);
+
+        Character nilaiA = 'Z';
+        switch (nilaiA) {
             case 'A':
-                System.out.println("Anda lulus sempurna");
+                System.out.println("Anda lolos");
                 break;
             case 'B':
-                System.out.println("Anda Lulus!");
+                System.out.println("Anda lulus percobaan");
+                break;
+            case 'C':
+                System.out.println("Anda bodoh");
                 break;
             default:
-                System.out.println("Anda salah masuk!");
+                System.out.println("Anda waras?");
                 break;
         }
 
-        /// Switch Lambda
-        switch (nilai) {
-            case 'A' -> System.out.println("Anda lulus!");
-            default -> {
-                System.out.println("Anda tidak lulus!");
-            }
-        }
-
-        /// Keyword yield
-        String ucapan = switch (nilai) {
-            case 'A':
-                yield "Wah, anda lulus sempurna hebat sekali!";
-            default:
-                yield "Okelah!";
-        };
-
-        System.out.println(ucapan);
     }
 }
